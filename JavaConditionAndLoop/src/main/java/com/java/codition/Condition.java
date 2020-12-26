23c6c3360647004e90ca0746b1c736631ed8f8d6
mahbu <mahbu@DESKTOP-MRK3TJ5.fios-router.home>
2020-12-25 23:05:36 -0500

    initial commit

---
+++ JavaConditionAndLoop/src/main/java/com/java/codition/Condition.java
@@ -0,0 +1,37 @@
+package com.java.codition;
+
+public class Condition {
+
+
+	
+	public static void getCondition() {
+	
+	String Leder = "farhen";
+if (Leder == "farhen"  ) {System.out.println(" I do this");}
+	
+else {System.out.println("I don't do this");}
+	}
+public static void getVacation() {
+	
+	int money = 20000;
+	
+	if (money > 20000) {System.out.println(" I will be going vacation");}
+	else if (money== 2005) { System.out.println( "I will buy car" );}
+	else {System.out.println("i do nothing");}
+
+}
+
+
+
+
+
+
+public static void main(String[] args) {
+	
+Condition.getCondition();
+Condition.getVacation();
+
+}
+
+
+}
---
+++ JavaConditionAndLoop/src/main/java/com/java/codition/JavaLoop.java
@@ -0,0 +1,17 @@
+package com.java.codition;
+
+public class JavaLoop {
+
+public static void main(String[] args) {
+
+// need to print 0 to 1000;	
+	
+for (int a = 0 ; a <=1000; a++) {System.out.println(a);}
+	
+for (int b = 100; b >=0; b--) {System.out.println(b);}
+
+}
+
+
+
+}
